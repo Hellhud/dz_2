1fd458f2265f039a923284bf0c5c69c637edd42a
Hellhound <hardoneloves@gmail.com>
2021-11-29 17:28:43 +0300

    Добавьте файлы проекта.

---
+++ dz_2/dz_2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+void tt(int a[4][4]) {
+	for (int i = 0; i < 4; i++) {
+		for (int u = 0; u < 4; u++) {
+			a[i][u] = rand() % 10;
+		}
+	}
+}
+void vv(int a[4][4]) {
+	for (int i = 0; i < 4; i++) {
+		cout << endl;
+		for (int u = 0; u < 4; u++) {
+			cout << a[i][u] << " ";
+		}
+	}
+}
+int main() {
+	int a[4][4];
+	tt(a);
+	vv(a);
+}
